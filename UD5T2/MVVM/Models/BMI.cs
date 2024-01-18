f0ec6593d44b47a0476c655d6a32c35ccc879c59
fsanperDev <fsanperdev@gmail.com>
2024-01-18 09:14:48 +0100

    Agregar archivos de proyecto.

---
+++ UD5T2/MVVM/Models/BMI.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace UD5T2.MVVM.Models
+{
+    
+    public class BMI : INotifyPropertyChanged
+    {
+        private float altura;
+        private float peso;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Establecemos la propiedad Altura
+        public float Altura
+        {
+            get { return altura; }
+            set
+            {
+                altura = value;
+                OnPropertyChanged(nameof(Altura));
+                OnPropertyChanged(nameof(Resultado));
+                OnPropertyChanged(nameof(ResultadoBMI));
+            }
+        }
+
+        // Establecemos la propiedad Peso
+        public float Peso
+        {
+            get { return peso; }
+            set
+            {
+                peso = value;
+                OnPropertyChanged(nameof(Peso));
+                OnPropertyChanged(nameof(Resultado));
+                OnPropertyChanged(nameof(ResultadoBMI));
+            }
+        }
+
+        // Establecemos la propiedad Resultado
+        public float Resultado
+        {
+            // Definimos el get que nos devolvera el resultado obtenido según el peso y la altura
+            get
+            {
+                return Peso / (Altura * Altura) * 10000;
+            }
+        }
+
+        // Establecemos la propiedad Resultado
+        public String ResultadoBMI
+        {
+            // Definimos el get que nos devolvera un mensaje según el resultado obtenido
+            get
+            {
+                if (Resultado <= 16 )
+                {
+                    return "BMI: Delgado Severo";
+                } 
+                else if (Resultado <= 17)
+                {
+                    return "BMI: Delgado Moderado";
+                } 
+                else if (Resultado <= 18.5)
+                {
+                    return "BMI: Delgado Medio";
+                }
+                else if (Resultado <= 25)
+                {
+                    return "BMI: Normal";
+                }
+                else if (Resultado <= 30)
+                {
+                    return "BMI: Sobrepeso";
+                }
+                else if (Resultado <= 35)
+                {
+                    return "BMI: Obesiddad Clase I";
+                }
+                else if (Resultado <= 40)
+                {
+                    return "BMI: Obesidad Clase II";
+                }
+                else
+                {
+                    return "BMI: Obesidad Clase III";
+                }
+
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+    }
+}
---
+++ UD5T2/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UD5T2.MVVM.Models;
+
+namespace UD5T2.MVVM.ViewModels
+{
+    internal class BMIViewModel
+    {
+        public BMI BMI
+        {
+            get; set;
+        }
+
+        public BMIViewModel()
+        {
+            BMI = new BMI();
+            BMI.Peso = 50;
+            BMI.Altura = 25;
+        }
+    }
+}
